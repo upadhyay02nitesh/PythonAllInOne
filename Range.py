f18afcb19fa4c9515a724cd869df45b680a14037
Nitesh <upadhyay02nitesh@example.com>
2023-12-22 20:56:35 +0530

    add

---
+++ Range.py
@@ -0,0 +1,22 @@
+a=range(10)
+print(a)
+for i in a:
+    print(i,end=" ")
+
+print()
+
+s=range(10,-1,-1)
+for i in s:
+    print(i,end=" ")
+
+print()
+
+c=range(-10,3,2)
+for i in c:
+    print(i,end=" ")
+
+print()
+
+c=range(-5,-3)
+for i in c:
+    print(i,end=" ")
